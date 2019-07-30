3f2bc66b82f3fb572c828a60ecb50a99e710c344
jcraig1730 <jcraig1730@gmail.com>
2019-07-29 20:40:31 -0700

    video list entry spec passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import exampleVideoData from './data/exampleVideoData.js';
+// import Search from './components/Search.jsx'
+ReactDOM.render(<App />, document.getElementById('app'));
 
